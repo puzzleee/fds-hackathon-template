1b1edddb670f323ff76a5220a4a469a1b35f35ec
Lee Jaeyeon <1290jaeyeonlee@gmail.com>
2018-05-10 01:09:39 +0900

    게임판 숫자를 랜덤으로 생성하는 함수 추가

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import './index.scss';
 class puzzle {
 // 상태
 // 게임 판은 숫자 셀 0~15
-  board = [
-    ['1', '2', '3', '4'],
-    ['5', '6', '7', '8'],
-    ['9', '10', '11', '12'],
-    ['13', '14', '15', '0']
-    ];
+  board = newArr();
 
 // 동작
 // 빈 셀의 상하좌우 중 하나를 클릭하면 빈 셀의 위치와 같아진다.
@@ -43,9 +38,9 @@ const rowEls = document.querySelectorAll('.board__row');
 rowEls.forEach((rowEl, rowIndex) => {
   const colEls = rowEl.querySelectorAll('.board__col');
   colEls.forEach((colEl, colIndex) => {
+    draw(); // 랜덤 생성된 배열의 숫자를 셀에 채워넣기
     colEl.addEventListener('click', e => {
       game.turn({row: rowIndex, col: colIndex});
-      draw();
     })
   })
 })
@@ -56,10 +51,34 @@ function draw() {
     const colEls = rowEl.querySelectorAll('.board__col');
     rowArr.forEach((col, colIndex) => {
       colEls[colIndex].textContent = col;
+      console.log(colEls[colIndex].textContent);
     })
   })
   const finish = game.checkResult();
   if (finish) {
     document.querySelector('.finish').textContent = finish;
   }
+}
+
+
+// 랜덤으로 배열 생성하는 함수
+function newArr() {
+  const numArr = [...Array(16).keys()]; // 0부터 15까지의 배열 생성
+  const boardArr =  [
+      [null, null, null, null],
+      [null, null, null, null],
+      [null, null, null, null],
+      [null, null, null, null]
+    ]; // 4 * 4 비어있는 배열
+  function randomNumber(numArr){
+    return Math.floor(numArr.length * Math.random());
+  }  
+  for(let i = 0 ; i < boardArr.length; i++){
+    for(let j = 0 ; j < boardArr.length; j++){
+      const randomNum = randomNumber(numArr);
+      boardArr[i][j] = numArr[randomNum];
+      numArr.splice(numArr.indexOf(numArr[randomNum]), 1); 
+    }
+  }
+  return boardArr;
 }
